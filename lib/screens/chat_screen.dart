4d6b153222f213e31f94868335d993b30052a3c7
Deepeshac2890 <deepeshac290@gmail.com>
2021-04-02 21:39:39 +0530

    Initial Commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,59 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
+  static String id = 'ChatScreen';
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
+final fs = Firestore.instance;
+FirebaseUser loggedInUser;
+
 class _ChatScreenState extends State<ChatScreen> {
+  final FirebaseAuth fu = FirebaseAuth.instance;
+  final messageTextController = TextEditingController();
+  String msgTxt;
+
+  void sendMessage() async {
+    try {
+      await fs
+          .collection('messages')
+          .add({'text': msgTxt, 'sender': loggedInUser.email});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void currentUser() async {
+    try {
+      final user = await fu.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentUser();
+  }
+
+  void logout() async {
+    try {
+      await fu.signOut();
+      Navigator.pop(context);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +61,9 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+              icon: Icon(Icons.logout),
               onPressed: () {
-                //Implement logout functionality
+                logout();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -27,6 +74,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -34,15 +82,17 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        msgTxt = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      messageTextController.clear();
+                      sendMessage();
                     },
                     child: Text(
                       'Send',
@@ -54,6 +104,96 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: fs.collection('messages').snapshots(),
+        builder: (context, snapshot) {
+          List<MessageBubble> messageBubbles = [];
+          if (snapshot.hasData != null) {
+            // snapshot is async snapshot from Flutter
+            // Reversed is added to reverse the order of the list so new message goes
+            // the bottom
+            final messages = snapshot.data.documents.reversed;
+            for (var message in messages) {
+              // message is document snapshot from firebase
+              // messageText is map that has key:value pair
+              final messageText = message.data['text'];
+              final messageSender = message.data['sender'];
+              final currentUser = loggedInUser.email;
+
+              final messageBubble = MessageBubble(
+                text: messageText,
+                sender: messageSender,
+                isMe: currentUser == messageSender ? true : false,
+              );
+              messageBubbles.add(messageBubble);
+            }
+            return Expanded(
+              child: ListView(
+                // This makes listview sticky to bottom of listview
+                reverse: true,
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+                children: messageBubbles,
+              ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String sender;
+  final String text;
+  final bool isMe;
+  double topLeft;
+  double topRight;
+  Color colorDef;
+  CrossAxisAlignment cal;
+
+  MessageBubble({this.sender, this.text, this.isMe});
+  @override
+  Widget build(BuildContext context) {
+    topLeft = isMe ? 30.0 : 0.0;
+    topRight = isMe ? 0.0 : 30.0;
+    colorDef = isMe ? Colors.white : Colors.blue;
+    cal = isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: cal,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(topLeft),
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+              topRight: Radius.circular(topRight),
+            ),
+            color: colorDef,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text('$text'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,96 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/Components/PaddingWidgetReusable.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static String id = 'RegistrationScreen';
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  // Firebase auth instance is created
+  final FirebaseAuth fu = FirebaseAuth.instance;
+  String emailId;
+  String password;
+  bool showSpinner = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  emailId = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Paddy(
+                      op: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          final newUser =
+                              await fu.createUserWithEmailAndPassword(
+                                  email: emailId, password: password);
+                          if (newUser != null) {
+                            //success
+                            setState(() {
+                              showSpinner = false;
+                            });
+
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          }
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      textVal: 'Register',
+                      bColor: Colors.blue)
+                  .getPadding(),
+            ],
+          ),
         ),
       ),
     );
